413c3ba0efacd465929befab181465c74cf54a66
Steve Bishop <stxxop@gmail.com>
2019-03-29 20:35:57 -0700

    Add project files.

---
+++ Northwind.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Northwind.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new Northwind.Services.Data.NorthwindDataContext())
+            {
+                var productList = ctx.Products.ToList();
+                foreach (var product in productList)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+
+                var catList = ctx.Categories.Include(c => c.Products).ToList();
+                foreach (var cat in catList)
+                {
+                    Console.WriteLine(cat.CategoryName);
+
+                    foreach (var prod in cat.Products)
+                    {
+                        Console.WriteLine("--" + prod.ProductName);
+                    }
+                }
+            }
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Northwind.Web/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: /products/
+        [HttpGet(Name = nameof(GetAll))]
+        public IEnumerable<Product> GetAll()
+        {
+            IEnumerable<Product> productList;
+            using (var ctx = new Northwind.Services.Data.NorthwindDataContext())
+            {
+                productList = ctx.Products.ToList();
+            }
+            return productList;
+        }
+
+        // GET: /products/search/{value}
+        [HttpGet("search/{value}", Name = nameof(GetBySearch))]
+        public IEnumerable<Product> GetBySearch(string value)
+        {
+            IEnumerable<Product> productList;
+            using (var ctx = new Northwind.Services.Data.NorthwindDataContext())
+            {
+                productList = ctx.Products.Where(x=>x.ProductName.Contains(value)).ToList();
+            }
+            return productList;
+        }
+
+        // GET: /products/category/{categoryId}
+        [HttpGet("category/{categoryId}", Name = nameof(GetByCategoryId))]
+        public IEnumerable<Product> GetByCategoryId(int categoryId)
+        {
+            IEnumerable<Product> productList;
+            using (var ctx = new Northwind.Services.Data.NorthwindDataContext())
+            {
+                productList = ctx.Products.Where(x => x.CategoryId == categoryId).ToList();
+            }
+            return productList;
+        }
+
+
+        // GET: /products/{productId}
+        [HttpGet("{productId}", Name = nameof(GetByProductId))]
+        public Product GetByProductId(int productId)
+        {
+            using (var ctx = new Northwind.Services.Data.NorthwindDataContext())
+            {
+                return ctx.Products.FirstOrDefault(x => x.ProductId == productId);
+            }
+        }
+
+        // POST: /products/{product}
+        [HttpPost]
+        public void Post([FromBody] Product value) => throw new NotImplementedException();
+
+        // PUT: /products/{product}
+        [HttpPut]
+        public void Put([FromBody] Product value) => throw new NotImplementedException();
+
+        // DELETE: /products/{productId}
+        [HttpDelete("{productId}")]
+        public void Delete(int productId) => throw new NotImplementedException();       
+    }
+}
